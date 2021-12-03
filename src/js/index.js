bb97dfb0e7047dd47d9e9a964080e28767e8b3a3
verenichyi <verenichyi@gmail.com>
2021-12-03 16:05:02 +0300

    imported swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,45 +1,9 @@
-import Swiper, {Navigation, Pagination} from 'swiper';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-Swiper.use([Navigation, Pagination]);
-
-const swiper = new Swiper('.swiper', {
-	slidesPerView: 7,
-	loop: true,
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-	breakpoints: {
-		320: {
-			slidesPerView: 2,
-		},
-		480: {
-			slidesPerView: 3,
-		},
-		1024: {
-			slidesPerView: 4,
-		},
-		1500: {
-			slidesPerView: 5,
-		},
-		2000: {
-			slidesPerView: 6,
-		},
-		2400: {
-			slidesPerView: 7,
-		}
-	}
-});
-
+import {swiper} from './swiper';
 
 const navigationBlock = document.getElementById('navigation');
 const menuList = document.getElementById('menu-list');
 const menuIcon = document.getElementById('menu-icon');
 const navMenu = document.getElementById('nav-menu');
-
 
 const onMenuIconClick = () => {
 	document.body.classList.toggle('lock');
